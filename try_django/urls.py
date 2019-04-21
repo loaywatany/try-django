82341606ab7e73b82cc5076c47f8058763ceffd1
Hesham Watany <hesham.watany@gmail.com>
2019-04-21 05:15:05 +0200

    Added views.py with functions (home, about, contact pages) & added new folder templates with hello world html file

--- try_django/urls.py
+++ try_django/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from .views import (
+    home_page,
+    about_page,
+    contact_page
+    )
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'pages?/$', about_page),
+    re_path(r'about/$', about_page),
+    path('contact/', contact_page),
+    path('', home_page),
+
 ]
