4130b73a52846608742451f9d715b59b80d8acf7
Hesham Watany <hesham.watany@gmail.com>
2019-04-21 05:28:43 +0200

    Added the base_dir map

--- try_django/settings.py
+++ try_django/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'try_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'try_django/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
