dd119d88d22984dd5a2abd8de9f0a0466333b83e
Hesham Watany <hesham.watany@gmail.com>
2019-04-29 06:28:40 +0200

    Added new comment for more details

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from .models import BlogPost
 
 
 def blog_post_detail_page(request):
-    obj = BlogPost.objects.get(id=1)
+    obj = BlogPost.objects.get(id=1) #query -> database -> data -> django renders it
     template_name = 'blog_post_detail.html'
     context = {"object": obj}
     return render(request, template_name, context)
