e8e689c20885edd4ec99798f311a2959dbb81013
Hesham Watany <hesham.watany@gmail.com>
2019-05-01 08:54:43 +0200

    Added login required

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+
 from django.http import Http404
 from django.shortcuts import render,get_object_or_404
 
@@ -12,6 +14,8 @@ def blog_post_list_view(request):
     context = {"object_list": qs}
     return render(request, template_name, context)
 
+
+@login_required(login_url='/login')
 
 def blog_post_create_view(request):
     form = BLogPostModelForm(request.POST or None)
