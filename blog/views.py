60fc5cbfa640a362248e43f30c4818b13af7327b
Hesham Watany <hesham.watany@gmail.com>
2019-05-01 06:59:38 +0200

    All templates renamed

--- blog/views.py
+++ blog/views.py
@@ -7,33 +7,33 @@ from .models import BlogPost
 
 def blog_post_list_view(request):
     qs = BlogPost.objects.all()
-    template_name = 'blog_post_list_view.html'
+    template_name = 'list.html'
     context = {"object_list": qs}
     return render(request, template_name, context)
 
 
 def blog_post_create_view(request):
-    template_name = 'blog_post_create.html'
+    template_name = 'create.html'
     context = {"form": None}
     return render(request, template_name, context)
 
 
 def blog_post_detail_view(request, post_id):
     obj = get_object_or_404(BlogPost, id=post_id)
-    template_name = 'blog_post_detail.html'
+    template_name = 'detail.html'
     context = {"object": obj}
     return render(request, template_name, context)
 
 
 def blog_post_update_view(request, post_id):
     obj = get_object_or_404(BlogPost, id=post_id)
-    template_name = 'blog_post_update.html'
+    template_name = 'update.html'
     context = {"object": obj, "form": None}
     return render(request, template_name, context)
 
 
 def blog_post_delete_view(request, post_id):
     obj = get_object_or_404(BlogPost, id=post_id)
-    template_name = 'blog_post_delete.html'
+    template_name = 'delete.html'
     context = {"object": obj}
     return render(request, template_name, context)
