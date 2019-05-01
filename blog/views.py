96060dedf073c2e78e4a42b312ab75c393eafe23
Hesham Watany <hesham.watany@gmail.com>
2019-05-01 05:12:14 +0200

    Added the variables

--- blog/views.py
+++ blog/views.py
@@ -14,20 +14,33 @@ def blog_post_detail_page(request, post_id):
 
 
 def blog_post_list_view(request):
-    return
+    template_name = 'blog_post_list.html'
+    context = {"object_list": []}
+    return render(request, template_name, context)
 
 
 def blog_post_create_view(request):
-    return
+    template_name = 'blog_post_create.html'
+    context = {"form": None}
+    return render(request, template_name, context)
 
 
-def blog_post_retrieve_view(request):
-    return
+def blog_post_detail_page(request, post_id):
+    obj = get_object_or_404(BlogPost, id=post_id)
+    template_name = 'blog_post_detail.html'
+    context = {"object": obj}
+    return render(request, template_name, context)
 
 
 def blog_post_update_view(request):
-    return
+    obj = get_object_or_404(BlogPost, id=post_id)
+    template_name = 'blog_post_update.html'
+    context = {"object": obj, "form": None}
+    return render(request, template_name, context)
 
 
 def blog_post_delete_view(request):
-    return
+    obj = get_object_or_404(BlogPost, id=post_id)
+    template_name = 'blog_post_delete.html'
+    context = {"object": obj}
+    return render(request, template_name, context)
